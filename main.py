d646ec21df5ca9a5d5b08cbe396c9ba49bbcb08d
isayan_58 <sayan.96.adhikary@gmail.com>
2022-11-25 02:23:38 +0530

    Push files for first time.

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StringType, IntegerType, StructType
+
+spark = SparkSession.builder.appName('Basics').getOrCreate()
+def print_hi(name):
+    print(f'Hi, from {name}.')
+    df = spark.read.json('people.json')
+    df.show()
+    df.printSchema()
+    df.describe().show()
+
+if __name__ == '__main__':
+    print_hi('Sayan')
+
